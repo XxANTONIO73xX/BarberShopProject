62e6790c59c317d1d02f6413cb299b8a4459798f
josejc456 <campaj456@gmail.com>
2022-04-04 16:01:37 -0700

    Index citas

---
+++ BarberShopClienteWebApp/BarberShop/Cita/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Citas</title>
+</head>
+<body>
+
+<h3> Tus citas </h3>
+    <table class="citas">
+        <tr>
+            <th> Barbería </th>
+            <th> Barbero </th>
+            <th> Corte </th>
+            <th> Fecha y Hora </th>
+            <th> Estado </tr>  
+        </tr>
+        <br>
+        <tr>
+            <td> "" </td>
+            <td> "" </td>
+            <td> "" </td>
+            <td> "" </td>
+            <td> "" </td>
+        </tr>
+
+        t
+    </table>
+</body>
+</html>
